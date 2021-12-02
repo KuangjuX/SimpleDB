9af2326d1d9bac6b26d7b9c969f1ab2e0d9b026d
KuangjuX <qcx@tju.edu.cn>
2021-12-02 06:28:45 -0800

    init

---
+++ src/concurrency/lock_manager.cpp
@@ -0,0 +1,42 @@
+//===----------------------------------------------------------------------===//
+//
+//                         BusTub
+//
+// lock_manager.cpp
+//
+// Identification: src/concurrency/lock_manager.cpp
+//
+// Copyright (c) 2015-2019, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#include "concurrency/lock_manager.h"
+
+#include <utility>
+#include <vector>
+
+namespace bustub {
+
+bool LockManager::LockShared(Transaction *txn, const RID &rid) {
+  txn->GetSharedLockSet()->emplace(rid);
+  return true;
+}
+
+bool LockManager::LockExclusive(Transaction *txn, const RID &rid) {
+  txn->GetExclusiveLockSet()->emplace(rid);
+  return true;
+}
+
+bool LockManager::LockUpgrade(Transaction *txn, const RID &rid) {
+  txn->GetSharedLockSet()->erase(rid);
+  txn->GetExclusiveLockSet()->emplace(rid);
+  return true;
+}
+
+bool LockManager::Unlock(Transaction *txn, const RID &rid) {
+  txn->GetSharedLockSet()->erase(rid);
+  txn->GetExclusiveLockSet()->erase(rid);
+  return true;
+}
+
+}  // namespace bustub
